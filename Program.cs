f734489894735751b226b2f23bd106f716f69ecf
cemil <gokalpcakar@gmail.com>
2021-06-20 20:04:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace patika_konsol_uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Adınızı giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz:");
+            string surName = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surName);
+        }
+    }
+}
